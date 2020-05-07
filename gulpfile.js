893d2651e4b0b57603744a0171a7a545a65c85e3
Maria <maria131621@yandex.ru>
2020-05-07 12:27:30 +0300

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,34 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 
-gulp.task('hello', function(done){
-  console.log('Привет, мир!');
-  done();
-});
-
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
-gulp.task('minify-css', () => {
-  return gulp.src('css/*.css')
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
+
+function minifyCSS() {
+  return src('css/*.css')
     .pipe(cleanCSS())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(dest('dist'));
+};
