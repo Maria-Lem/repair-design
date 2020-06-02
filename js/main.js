816c2b7b2e9807b253c52c5b35cdd1e56877dceb
Maria <maria131621@yandex.ru>
2020-06-02 15:25:23 +0300

    refact: rewrite JS in main.js to jQuery

--- js/main.js
+++ js/main.js
@@ -1,24 +1,57 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  };
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  closeBtn.addEventListener('click', switchModal);
+// document.addEventListener('DOMContentLoaded', function(){
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   };
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+//   closeBtn.addEventListener('click', switchModal);
   
-  window.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      modal.classList.remove('modal--visible');
+//   window.addEventListener('keydown', (event) => {
+//     if (event.key === 'Escape') {
+//       switchModal();
+//     }
+//   });
+  
+//   window.addEventListener("click", e => {
+//     if (e.target == modal) {
+//       switchModal();
+//     }
+//   });
+// });
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+  
+  modalBtn.on('click', function(){
+    modal.toggleClass('modal--visible');
+  });
+
+  closeBtn.on('click', function(){
+    modal.toggleClass('modal--visible');
+  });
+
+  $(document).on('keydown', function(e){
+    if (e.key === 'Escape'){
+      modal.removeClass('modal--visible');
     }
   });
-  
-  window.addEventListener("click", e => {
-    if (e.target == modal) {
-      modal.classList.remove('modal--visible');
+
+  $(document).click(function (e) {
+    if ($(e.target).is('.modal')) {
+      modal.removeClass('modal--visible');
     }
   });
+
+  // $(window).on('click', function(){
+  //   if (modal.hasClass('modal--visible')) {
+  //     modal.removeClass('modal--visible');
+  // }
+  // });
+  
 });
